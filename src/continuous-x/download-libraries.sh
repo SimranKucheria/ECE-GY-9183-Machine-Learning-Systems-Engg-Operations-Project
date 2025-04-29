dcba461a2411e0fe00b0f5e5f8c5c958a220d06a
Simran Kucheria <simran.kucheria@gmail.com>
2025-04-29 14:02:34 -0400

    Edit paths

--- src/continuous-x/download-libraries.sh
+++ src/continuous-x/download-libraries.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git clone --recurse-submodules https://github.com/SimranKucheria/ECE-GY-9183-Machine-Learning-Systems-Engg-Operations-Project.git /work/deeptrust
 
 mkdir -p /work/.local/bin
 wget https://releases.hashicorp.com/terraform/1.10.5/terraform_1.10.5_linux_amd64.zip
@@ -7,9 +6,9 @@ unzip -o -q terraform_1.10.5_linux_amd64.zip
 mv terraform /work/.local/bin
 rm terraform_1.10.5_linux_amd64.zip
 
-cp clouds.yaml /work/deeptrust/tf/kvm/clouds.yaml
-cp clouds.yaml /work/deeptrust/tf/tacc/clouds.yaml
-cp clouds.yaml /work/deeptrust/tf/uc/clouds.yaml
+cp clouds.yaml /work/deeptrust/src/continuous-x/tf/kvm/clouds.yaml
+cp clouds.yaml /work/deeptrust/src/continuous-x/tf/tacc/clouds.yaml
+cp clouds.yaml /work/deeptrust/src/continuous-x/tf/uc/clouds.yaml
 
 export PATH=/work/.local/bin:$PATH
 
@@ -18,6 +17,6 @@ PYTHONUSERBASE=/work/.local pip install --user ansible-core==2.16.9 ansible==9.8
 export PATH=/work/.local/bin:$PATH
 export PYTHONUSERBASE=/work/.local
 
-PYTHONUSERBASE=/work/.local pip install --user -r /work/deeptrust/ansible/k8s/kubespray/requirements.txt
+PYTHONUSERBASE=/work/.local pip install --user -r /work/deeptrust/src/continuous-x/ansible/k8s/kubespray/requirements.txt
 
 
--- src/continuous-x/provision-terraform.sh
+++ src/continuous-x/provision-terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /work/deeptrust/tf/kvm
+cd /work/deeptrust/src/continuous-x/tf/kvm
 export PATH=/work/.local/bin:$PATH
 unset $(set | grep -o "^OS_[A-Za-z0-9_]*")
 terraform init
@@ -10,13 +10,13 @@ terraform validate
 terraform plan
 terraform apply -auto-approve
 KVM_FIP=$(terraform output -raw floating_ip_out)
-cd /work/deeptrust/tf/uc
+cd /work/deeptrust/src/continuous-x/tf/tacc
 terraform init
 terraform validate
 terraform plan
 terraform apply -auto-approve
 UC_FIP=$(terraform output -raw floating_ip_out)
-cd /work/deeptrust/tf/tacc
+cd /work/deeptrust/src/continuous-x/tf/tacc
 unset $(set | grep -o "^OS_[A-Za-z0-9_]*")
 terraform init
 terraform validate
@@ -25,7 +25,7 @@ terraform apply -auto-approve
 TACC_FIP=$(terraform output -raw floating_ip_out)
 
 var1="A.B.C.D"
-sed -i 's/'"$var1"'/'"$KVM_FIP"'/g' /work/deeptrust/ansible/ansible.cfg
+sed -i 's/'"$var1"'/'"$KVM_FIP"'/g' /work/deeptrust/src/continuous-x/ansible/ansible.cfg
 
 export $KVM_IP
 export $UC_IP
